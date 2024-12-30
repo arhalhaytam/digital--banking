67a0e0dfd9fb329f6168ed27e84f8a73f5eb5d52
Bazaglo <153495859+arhalhaytam@users.noreply.github.com>
2024-12-30 14:35:56 +0100

    Delete TP3_Hospital directory

--- TP3_Hospital/src/main/java/ma/mundiapolis/tp3_hospital/repositories/ConsultationRepository.java
+++
@@ -1,7 +0,0 @@
-package ma.mundiapolis.tp3_hospital.repositories;
-
-import ma.mundiapolis.tp3_hospital.entities.Consultation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
-}
